0891488dd69ba728464acdcd341a6df45a50411c
HARISH S <harishofficial315@gmail.com>
2026-03-21 14:53:28 +0530

    back

--- backend/index.js
+++ backend/index.js
@@ -68,6 +68,10 @@ app.post("/sendemail", function (req, res) {
 
 })
 
+app.get("/",(req,res) => {
+    res.send("Backend is running...");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>{
